c27d610556c6d6bc8187b1361e0d58d7f24abec4
steven barny <steven.barny@gmail.com>
2023-11-10 17:09:16 +0100

    filereader done

--- src/main/java/com/ekinox/bookshop/BookshopApplication.java
+++ src/main/java/com/ekinox/bookshop/BookshopApplication.java
@@ -1,13 +1,21 @@
 package com.ekinox.bookshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookshopApplication {
-
+public class BookshopApplication implements ApplicationRunner {
+	@Autowired
+	DVDService dvdService;
 	public static void main(String[] args) {
 		SpringApplication.run(BookshopApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) {
+		dvdService.listDVDFromFile("cart.txt");
+	}
 }
